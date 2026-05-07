0bb6772ae564af5f5d114b5ae1f90fc6d80adca2
Axel-Wolkowicz <wolkowiczaxel@gmail.com>
2026-05-07 10:18:39 -0300

    update movement

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    int posicion = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +14,13 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
+        if((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && posicion < 1) {
             transform.Translate(2,0,0);
+            posicion++;
         }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
+        else if((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && posicion > -1) {
             transform.Translate(-2,0,0);
+            posicion--;
         }
     }
 }
